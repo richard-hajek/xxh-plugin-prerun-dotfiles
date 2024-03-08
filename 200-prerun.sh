6847458ac2fd0bf0ac7228c09794f747a4f989bc
Richard Hajek <richard.m.hajek@gmail.com>
2024-03-08 17:35:26 +0100

    update

--- 200-prerun.sh
+++ 200-prerun.sh
@@ -3,13 +3,13 @@ echo Running ver 2
 mkdir -p ~/.cache/bash
 
 if hash git &> /dev/null; then
-    echo sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply richard-hajek
+    sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply richard-hajek
 
     if hash bash &> /dev/null; then
-        echo git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+        git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
     fi
 
     if hash zsh &> /dev/null; then
-        echo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
 fi
---
+++ reinstall.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "update"
+git push
+xxh +RI https://github.com/richard-hajek/xxh-plugin-prerun-dotfiles
