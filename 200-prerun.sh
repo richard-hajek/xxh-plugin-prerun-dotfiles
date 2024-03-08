5a79b4db4c6c72fde51448eb6565ef36ef3235c7
Richard Hajek <richard.m.hajek@gmail.com>
2024-03-08 17:18:16 +0100

    fast

--- 200-prerun.sh
+++ 200-prerun.sh
@@ -4,13 +4,13 @@ echo Running ver 2
 mkdir -p ~/.cache/bash
 
 if hash git &> /dev/null; then
-    sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply richard-hajek
+    echo sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply richard-hajek
 
     if hash bash &> /dev/null; then
-        git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+        echo git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
     fi
 
     if hash zsh &> /dev/null; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
 fi
